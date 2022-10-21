b72fbb95da3033d6c1ad851caf939d7bf2fe4339
Dariusz Okoński <darek200180@gmail.com>
2022-10-21 17:43:09 +0200

    add config file for firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBSXg22aYejafqc0u2Lp7mpoW06FrUax-g',
+  authDomain: 'the-dojo-v1.firebaseapp.com',
+  projectId: 'the-dojo-v1',
+  storageBucket: 'the-dojo-v1.appspot.com',
+  messagingSenderId: '1081229495182',
+  appId: '1:1081229495182:web:ce782a06a096757c8e72df',
+}
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp }
