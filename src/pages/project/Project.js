8a4f063b7b48bf351e95e481e1623535f134eee2
Dariusz Okoński <darek200180@gmail.com>
2022-11-04 11:44:58 +0100

    add error to useDocument

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,7 +1,28 @@
 import "./Project.css";
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
+import Loading from "../../components/Loading";
 
 function Project() {
-  return <div>Project details</div>;
+  const { id } = useParams();
+  const { document, error } = useDocument("projects", id);
+
+  console.log("document", document);
+  console.log("error", error);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  if (!document) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="project-details">
+      <h1>{document.name}</h1>
+    </div>
+  );
 }
 
 export default Project;
