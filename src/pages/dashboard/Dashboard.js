19af273de9ca0ccf784b08cb77840284c0305e3f
Dariusz Okoński <darek200180@gmail.com>
2022-11-07 15:43:26 +0100

    add filtering functionality

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -3,8 +3,10 @@ import { useCollection } from "../../hooks/useCollection";
 import ProjectList from "../../components/ProjectList";
 import ProjectFilter from "./ProjectFilter";
 import { useState } from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 function Dashboard() {
+  const { user } = useAuthContext();
   const { documents, error } = useCollection("projects");
   const [currentFilter, setCurrentFilter] = useState("all");
 
@@ -12,7 +14,30 @@ function Dashboard() {
     setCurrentFilter(newFilter);
   };
 
-  console.log(documents);
+  const projects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case "all":
+            return true;
+          case "mine":
+            let assignedToMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (user.uid === u.id) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          case "development":
+          case "design":
+          case "sales":
+          case "marketing":
+            console.log(document.category, currentFilter);
+            return document.category === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
 
   return (
     <div>
@@ -24,7 +49,7 @@ function Dashboard() {
           changeFilter={changeFilter}
         />
       )}
-      {documents && <ProjectList projects={documents} />}
+      {projects && <ProjectList projects={projects} />}
     </div>
   );
 }
