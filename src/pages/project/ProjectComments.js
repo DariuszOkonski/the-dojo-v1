49dc92539530ddd368afad538527c124578fd965
Dariusz Okoński <darek200180@gmail.com>
2022-11-08 09:54:11 +0100

    add date-fns package

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -4,6 +4,7 @@ import { useAuthContext } from "../../hooks/useAuthContext";
 import { v4 as uuidv4 } from "uuid";
 import { useFirestore } from "../../hooks/useFirestore";
 import Avatar from "../../components/Avatar";
+import { formatDistanceToNow } from "date-fns";
 
 function ProjectComments({ project }) {
   const { updateDocument, response } = useFirestore("projects");
@@ -43,7 +44,11 @@ function ProjectComments({ project }) {
                 <p>{comment.displayName}</p>
               </div>
               <div className="comment-date">
-                <p>date</p>
+                <p>
+                  {formatDistanceToNow(comment.createdAt.toDate(), {
+                    addSuffix: true,
+                  })}
+                </p>
               </div>
               <div className="comment-content">
                 <p>{comment.content}</p>
